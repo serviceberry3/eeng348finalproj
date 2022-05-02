c6aa14484179709e430bdcc56b82e27b95e5cb83
Noah <noah.weiner@yale.edu>
2022-05-02 04:35:38 -0400

    implement backspace

--- morse_RX/morse_RX.ino
+++ morse_RX/morse_RX.ino
@@ -1,12 +1,16 @@
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
+SoftwareSerial HC12(10, 11); //HC-12 TX Pin, HC-12 RX Pin
 int LED = 3;
 LiquidCrystal_I2C lcd(0x27,20,4);
 
-//#define push1 8
-//#define push2 7
+enum state {
+  LETTER,
+  LAT,
+  LON
+};
+
 const String A = ".-", B = "-...", C = "-.-.", D = "-..",
              E = ".", F = "..-.", G = "--.", H = "....",
              I = "..", J = ".---", K = "-.-", L = ".-..",
@@ -14,91 +18,91 @@ const String A = ".-", B = "-...", C = "-.-.", D = "-..",
              Q = "--.-", R = ".-.", S = "...", T = "-",
              U = "..-", V = "...-", W = ".--", X = "-..-",
              Y = "-.--", Z = "--..";
-String morse_letter = "";
-String message = "";             
+             
+String morse_letter = "";         
 int delay_time = 500;
-//void morse_dot;
-//void morse_dash;
 const unsigned long time_threshold = 250;
-//char space = "/";
 int debounce_delay = 50;
 
+String lat = "";
+String lon = "";
+
+state reader = LETTER;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   HC12.begin(9600); 
-  //pinMode(push1, INPUT);
-  //pinMode(push2, INPUT);
-  pinMode(LED,OUTPUT);
-  //lcd.init();                      // initialize the lcd
-  //lcd.backlight();
-  Serial.println("Start");
-  
+  lcd.init();              
+  lcd.backlight();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  //Serial.println("1");
- 
-  while (HC12.available()) {        // If HC-12 has data
-    //Serial.println("2");
+void loop() { 
+  while (HC12.available()) {
     char val = HC12.read();
-    Serial.println(val);
-    Serial.println(int(val));
-    morse_letter += val;
-    //analogWrite(LED,val);
-    if (int(val)==1){
-      Serial.print("Sending");
-      Serial.println(morse_letter);
-      message += morse_to_letter(morse_letter);
-      Serial.println(message);
-      morse_letter = "";
-    }
-  }
-/*
-  int state1, state2;
-  static int last_state1, last_state2;
-  static float start_time1, end_time1, time1;
-  state1 = digitalRead(push1);
-  state2 = digitalRead(push2);
- 
-  if(state1 == HIGH && last_state1 == LOW){
-    start_time1 = millis();
-  }
-  else if(state1 == LOW && last_state1 == HIGH){
-    end_time1 = millis();
-    time1 = end_time1 - start_time1;
-    if (time1 < time_threshold){
-      morse_letter += ".";
-      Serial.println(morse_letter);
-    }
-   
-    else {
-      morse_letter += "-";
-      Serial.println(morse_letter);
-    }
-  }
-  if (state2 == HIGH){
-    message += morse_to_letter(morse_letter);
-    Serial.println(morse_letter);
-    Serial.println(message);
-    morse_letter = "";
-    delay(delay_time);
-    lcd.clear();
-    lcd.print(message);
-  }
-  last_state1 = state1;
-  last_state2 = state2;
 
-  */
-  
+    switch(reader) {
+      //currently in morse letter reading state
+      case LETTER:
+        //a zero signifies the end of a letter
+        if (int(val) == 1) {
+          Serial.print("Receiving letter: ");
+          char letter = morse_to_letter(morse_letter);
+          
+          if (letter != 0) {
+            lcd.print(letter);
+            Serial.println(letter);
+          }
+          else {
+            Serial.println("UNKNOWN");
+          }
+            
+          morse_letter = "";
+
+          //should now be in latitude reading state
+          reader = LAT;
+        }
+        else if (int(val) == 2 && morse_letter != "") {
+          morse_letter.remove(morse_letter.length() - 1);
+        }
+        //otherwise we're reading the letter, so append dash or dot to the buffer string for current letter being read
+        else {
+          morse_letter += val;
+        }
+        break;
+
+      //currently in location reading state
+      case LAT:
+        //we've read in the location, so print it to display
+        if (int(val) == 1) {
+          Serial.print("Received lat: ");
+          Serial.println(lat);
+          reader = LON;
+          lat = "";
+        }
+        else {
+          lat += val;
+        }
+        break;
+      case LON:
+        //we've read in the location, so print it to display
+        if (int(val) == 1) {
+          Serial.print("Received lon: ");
+          Serial.println(lon);
+          reader = LETTER;
+          lon = "";
+        }
+        else {
+          lon += val;
+        }
+        break;
+    }
+  }  
 }
 
 
 char morse_to_letter(String letter)
 {
-  if(letter == A)      return 'A';
+  if (letter == A)      return 'A';
   else if(letter == B) return 'B';
   else if(letter == C) return 'C';
   else if(letter == D) return 'D';
@@ -124,5 +128,5 @@ char morse_to_letter(String letter)
   else if(letter == X) return 'X';
   else if(letter == Y) return 'Y';
   else if(letter == Z) return 'Z';
-  return ' ';
+  return 0;
 }
--- morse_TX/morse_TX.ino
+++ morse_TX/morse_TX.ino
@@ -1,11 +1,83 @@
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
-SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
-int LED = 3;
+#include <TinyGPSPlus.h>
+
+SoftwareSerial HC12(10, 11); //HC-12 TX Pin, HC-12 RX Pin
 LiquidCrystal_I2C lcd(0x27,20,4);
 
 #define push1 8
 #define push2 7
+
+static const int RXPin = 4, TXPin = 3; //rxpin should connect to pin labeled tx on gps module, and vice versa
+static const uint32_t GPSBaud = 9600; //GPS data rate
+
+uint32_t satellite_ctr = 0;
+
+String lat = "INVALID";
+String lon = "INVALID";
+
+//The TinyGPSPlus object
+TinyGPSPlus gps;
+
+//The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    lat = String(gps.location.lat(), 6);
+    lon = String(gps.location.lng(), 6);
+    Serial.print("Location: ");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(",");
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    lat = "INVALID";
+    lon = "INVALID";
+    Serial.print("Location: ");
+    Serial.print("INVALID");
+  }
+
+  Serial.print("  Date/Time: ");
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print("/");
+    Serial.print(gps.date.day());
+    Serial.print("/");
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print("INVALID");
+  }
+
+  Serial.print(" ");
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print("0");
+    Serial.print(gps.time.hour());
+    Serial.print(":");
+    if (gps.time.minute() < 10) Serial.print("0");
+    Serial.print(gps.time.minute());
+    Serial.print(":");
+    if (gps.time.second() < 10) Serial.print("0");
+    Serial.print(gps.time.second());
+    Serial.print(".");
+    if (gps.time.centisecond() < 10) Serial.print("0");
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print("INVALID");
+  }
+
+  Serial.println();
+}
+
 const String A = ".-", B = "-...", C = "-.-.", D = "-..",
              E = ".", F = "..-.", G = "--.", H = "....",
              I = "..", J = ".---", K = "-.-", L = ".-..",
@@ -13,74 +85,159 @@ const String A = ".-", B = "-...", C = "-.-.", D = "-..",
              Q = "--.-", R = ".-.", S = "...", T = "-",
              U = "..-", V = "...-", W = ".--", X = "-..-",
              Y = "-.--", Z = "--..";
+             
 String morse_letter = "";
 String message = "";             
 int delay_time = 500;
-//void morse_dot;
-//void morse_dash;
 const unsigned long time_threshold = 250;
 
-int debounce_delay = 50;
-int pot = A2;
+long debounce_delay = 100;
+long start_debounce_time = 0;
+
+static unsigned long last_interrupt_time = 0;
+
+static int last_state1, last_state2;
+
+boolean ignore_morse_button = false;
+  
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   HC12.begin(9600); 
-  pinMode(push1, INPUT);
-  pinMode(push2, INPUT);
-  pinMode(pot,INPUT);
 
-  lcd.init();                      // initialize the lcd
+  //button 1 (for the morse)
+  pinMode(push1, INPUT);
+
+  pinMode(push2, INPUT);
+ 
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+
+  digitalWrite(A3, HIGH);
+  digitalWrite(A2, LOW);
+  
+  lcd.init();                    
   lcd.backlight();
+
+  ss.begin(GPSBaud);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int val = map(analogRead(pot),0,1024,0,255);
-  //HC12.write(val); 
-  //Serial.println(val);
   int state1, state2;
-  static int last_state1, last_state2;
   static float start_time1, end_time1, time1;
+
+  //read in the pushbutton vals
   state1 = digitalRead(push1);
   state2 = digitalRead(push2);
- 
-  if(state1 == HIGH && last_state1 == LOW){
-    start_time1 = millis();
-  }
-  else if(state1 == LOW && last_state1 == HIGH){
-    end_time1 = millis();
-    time1 = end_time1 - start_time1;
-    if (time1 < time_threshold){
-      HC12.write(".");
-      morse_letter += ".";
-      Serial.println(morse_letter);
-    }
-   
-    else {
-       HC12.write("-");
-       morse_letter += "-";
-      //HC12.write(morse_letter);
-      Serial.println(morse_letter);
-      
-    }
-  }
-  if (state2 == HIGH){
-    //message += morse_to_letter(morse_letter);
-    //Serial.println(morse_letter);
-   // Serial.println(message);
-    HC12.write(1);
-    morse_letter = "";
-    delay(delay_time);
-   
-    //lcd.clear();
-    //lcd.print(message);
-  }
-  last_state1 = state1;
-  last_state2 = state2;
 
+  //only allow if debounce delay has passed
+  if (millis() > start_debounce_time + debounce_delay) {
+    start_debounce_time = millis();
+
+    //the morse button is being pressed down
+    if (state1 == HIGH && last_state1 == LOW) {
+      start_time1 = millis();
+    }
   
+    //the morse button is being released
+    else if (state1 == LOW && last_state1 == HIGH) {
+      end_time1 = millis();
+      time1 = end_time1 - start_time1;
+
+      if (ignore_morse_button == true) {
+        ignore_morse_button = false;
+      }
+      else {
+        //determine whether was dash or dot based on how long button held down
+        if (time1 < time_threshold) {
+          HC12.write(".");
+          morse_letter += ".";
+          lcd.print(".");
+        }
+       
+        else {
+           HC12.write("-");
+           morse_letter += "-";
+           lcd.print("-");
+        }
+      }
+    }
   
+    //the end signal is being given
+    if (state2 == HIGH && last_state2 == LOW) {
+      //if backspace request
+      if (state1 == HIGH && morse_letter != "") {
+        morse_letter.remove(morse_letter.length() - 1);
+        //Serial.println(morse_letter);
+        lcd.clear();
+        lcd.print(message);
+        lcd.print(morse_letter);
+        ignore_morse_button = true;
+
+        //write a backspace signal to the receiver
+        HC12.write(2);
+      }
+      else {
+        //write a 1 to signal end of letter
+        HC12.write(1);
+  
+        //send latitude
+        for (int i = 0; i < lat.length(); i++) {
+          HC12.write(lat[i]);
+        }
+        HC12.write(1);
+  
+        //send longitude
+        for (int i = 0; i < lon.length(); i++) {
+          HC12.write(lon[i]);
+        }
+        HC12.write(1);
+  
+        Serial.print("Sending letter: ");
+        Serial.print(morse_letter);
+  
+        char letter = morse_to_letter(morse_letter);
+  
+        Serial.print(" = ");
+        
+        if (letter != 0) {
+          Serial.println(letter);
+          message += letter;
+        }
+        else {
+          Serial.println("UNKNOWN");
+        }
+  
+        lcd.clear();
+        lcd.print(message);
+        
+        //clear letter
+        morse_letter = "";
+    
+        //delay half a second
+        delay(delay_time);
+      }
+    }
+    last_state2 = state2;
+    last_state1 = state1;
+  }
+
+
+  //GPS CODE
+  if (++satellite_ctr == 100000) {
+      Serial.print("Num satellites: ");
+      Serial.println(gps.satellites.value());
+  
+      while (ss.available() > 0) {
+        if (gps.encode(ss.read())) {
+          displayInfo();
+        }
+        else {
+          //Serial.println("gps encode() fail");
+        }
+    }
+      satellite_ctr = 0;
+  }
 }
 
 
@@ -112,5 +269,5 @@ char morse_to_letter(String letter)
   else if(letter == X) return 'X';
   else if(letter == Y) return 'Y';
   else if(letter == Z) return 'Z';
-  return ' ';
+  return 0;
 }
